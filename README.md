4e28026c70f421dca129607027f6dc634e46edfb
Yusuke Takahashi <yusuke_takahashi@cyberagent.co.jp>
2016-10-03 15:15:01 +0900

    Add README.md

---
+++ README.md
@@ -0,0 +1,1 @@
+http://sgykfjsm.github.io/blog/2016/03/13/golang-json-api-tutorial/
--- handlers.go
+++ handlers.go
@@ -13,8 +13,9 @@ func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 }
 
 func TodoIndex(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	todos := []Todo{ Todo{Name: "Write presentation"}, Todo{Name: "Host meetup"} }
-
+	todos := []Todo{ Todo{Name: "Write"}, Todo{Name: "Host meetup"} }
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	w.WriteHeader(http.StatusOK)
 	if err := json.NewEncoder(w).Encode(todos); err != nil {
 		panic(err)
 	}
