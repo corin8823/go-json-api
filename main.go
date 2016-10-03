e8e73118f124b9e6b3d80aab3888c40073974eb8
Yusuke Takahashi <yusuke_takahashi@cyberagent.co.jp>
2016-10-03 12:46:15 +0900

    Refactor main.go

--- main.go
+++ main.go
@@ -1,26 +1,11 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 
 	"github.com/julienschmidt/httprouter"
-	"encoding/json"
 )
-
-func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	fmt.Fprintf(w, "Welcome")
-}
-
-func TodoIndex(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	todos := []Todo{ Todo{Name: "Write presentation"}, Todo{Name: "Host meetup"} }
-	json.NewEncoder(w).Encode(todos)
-}
-
-func TodoShow(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	fmt.Fprintf(w, "TODO show: %s", ps.ByName("todoId"))
-}
 
 func main() {
 	router := httprouter.New()
